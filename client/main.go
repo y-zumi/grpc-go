834cda68b859623f67a4aced586f540d3d72c1a8
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-09-13 14:43:17 +0900

    add book service to clinet.go

--- client/main.go
+++ client/main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"time"
 
+	"github.com/bxcodec/faker/v3"
+	"github.com/y-zumi/grpc-go/proto/book"
 	"github.com/y-zumi/grpc-go/proto/user"
 	"google.golang.org/grpc"
 )
+
+const (
+	BookStatusLending = "Lending"
+)
+
+type BookService struct {
+	client user.UsersClient
+}
+
+func NewBookService(client user.UsersClient) *BookService {
+	return &BookService{
+		client: client,
+	}
+}
+
+func (s *BookService) FindLendingBookByID(ctx context.Context, req book.FindLendingBookByIDRequest) (*book.FindLendingBookByIDResponse, error) {
+	findByIDRequest := user.FindByIDRequest{
+		Id: faker.UUIDDigit(),
+	}
+	borrower, err := s.client.FindByID(ctx, &findByIDRequest)
+	if err != nil {
+		return nil, errors.New("user is not found error")
+	}
+
+	return &book.FindLendingBookByIDResponse{
+		Book: &book.Book{
+			Id:     req.Id,
+			Title:  faker.Word(),
+			Status: BookStatusLending,
+		},
+		Borrower: borrower.User,
+	}, nil
+}
 
 func main() {
 	// Set up a gRPC client
@@ -16,16 +52,19 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := user.NewUsersClient(conn)
+	usersClient := user.NewUsersClient(conn)
 
 	// Request to gRPC server
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.FindByID(ctx, &user.FindByIDRequest{
-		Id: "1",
+
+	bookService := NewBookService(usersClient)
+	resp, err := bookService.FindLendingBookByID(ctx, book.FindLendingBookByIDRequest{
+		Id: "123",
 	})
 	if err != nil {
-		log.Fatalf("could not find user: %v", err)
+		log.Fatal(err)
 	}
-	log.Printf("User: %v", r.User)
+
+	log.Printf("Book: %v", resp)
 }
